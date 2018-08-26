16b48f40e45ae2a5d73de8375f86c88f81e6df01
Joseph Venuto <jbvenuto@gmail.com>
2018-08-26 13:23:42 -0400

    Made the connection to the database in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,20 @@
 //Require the MySQL package
 var mysql = require("mysql");
 
+//Make a variable to hold the connection information and to export
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "pizza_db"
+});
+
+//Make the connection to the database
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+})
 
 //Export the connection for other files to use
 module.exports = connection;
